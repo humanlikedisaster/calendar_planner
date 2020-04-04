134319ba9824ae39d367cb7c64ee991f674cbb93
humanlikedisaster <humanlikedisaster@github.com>
2020-04-04 23:01:06 +0300

    Parameters update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ public func routes(_ router: Router) throws {
     // Example of configuring a controller
     let calendar = CalendarController()
     router.post(UserCreate.self, at: "create", use: calendar.create)
-    router.post(UserCreate.self, at: "join", use: calendar.join)
-    router.get("get_planned", use: calendar.getPlanned)
+    router.post(UserCreate.self, at: "join", String.parameter, use: calendar.join)
+    router.get("get_planned", String.parameter, use: calendar.getPlanned)
     router.post(User.self, at: "update", use: calendar.update)
 }
